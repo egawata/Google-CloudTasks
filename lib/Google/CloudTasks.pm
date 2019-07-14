03a9da4c3d0fe88f7a658ae79dccc85b5ae7aca4
egawata <egawa.takashi@gmail.com>
2019-07-15 06:45:55 +0900

    initial commit

---
+++ lib/Google/CloudTasks.pm
@@ -0,0 +1,39 @@
+package Google::CloudTasks;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::CloudTasks - It's new $module
+
+=head1 SYNOPSIS
+
+    use Google::CloudTasks;
+
+=head1 DESCRIPTION
+
+Google::CloudTasks is ...
+
+=head1 LICENSE
+
+Copyright (C) egawata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+egawata E<lt>egawa.takashi@gmail.comE<gt>
+
+=cut
+
